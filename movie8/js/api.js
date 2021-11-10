5f07c1345bcbd036a866df3cdec568b07e6d018e
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2021-11-10 15:18:33 +0800

    Add files via upload

--- movie8/js/api.js
+++ movie8/js/api.js
@@ -1,4 +1,4 @@
-function status(response); {
+function status(response) {
     if (response.status !== 200) {
         console.log("Error : " + response.status);
         return Promise.reject(new Error(response.statusText));
