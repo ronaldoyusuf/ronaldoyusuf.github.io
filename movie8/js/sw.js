32b81fe5d86de3ca8731643b221c6d1cef745d3a
RonaldoYusuf <90240652+ronaldoyusuf@users.noreply.github.com>
2021-11-22 21:12:20 +0800

    Add files via upload

--- movie8/js/sw.js
+++ movie8/js/sw.js
@@ -20,3 +20,35 @@ self.addEventListener("install", function(event) {
         }) 
     ); 
 });
+
+self.addEventListener("fetch", function(event) { 
+    event.respondWith( 
+        caches 
+        .match(event.request, { cacheName: CACHE_NAME }) 
+        .then(function(response) { if (response) { console.log("ServiceWorker: Gunakan aset dari cache: ", response.url); 
+        return response;
+    } 
+    
+    console.log( 
+        "ServiceWorker: Memuat aset dari server: ", 
+        event.request.url 
+        ); 
+        return fetch(event.request); 
+    }) 
+    ); 
+});
+
+self.addEventListener("activate", function(event) { 
+    event.waitUntil( 
+        caches.keys().then(function(cacheNames) { 
+            return Promise.all( 
+                cacheNames.map(function(cacheName) { 
+                    if (cacheName != CACHE_NAME) { 
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus"); 
+                        return caches.delete(cacheName); 
+                    } 
+                }) 
+                ); 
+            }) 
+            ); 
+        });
